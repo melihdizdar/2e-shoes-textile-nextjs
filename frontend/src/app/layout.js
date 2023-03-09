22f2e8c644dff19d7bab305b343d99b5607cff4b
Melih Dizdar <mlhdzdr@gmail.com>
2023-03-09 15:36:35 +0300

    (update): file structure

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,4 +1,6 @@
-import './globals.css'
+import Header from './components/common/Header'
+import Footer from './components/common/Footer'
+import '../styles/globals.css'
 
 export const metadata = {
   title: 'Create Next App',
@@ -8,7 +10,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <Header/>
+        {children}
+        <Footer />
+      </body>
     </html>
   )
 }
--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,6 +1,8 @@
+import '../styles/home.css';
+
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline test">
       Hello world!
     </h1>
   )
