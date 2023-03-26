3a5c76857d5dbb2eec87374466f9f62d932478c4
Melih Dizdar <mlhdzdr@gmail.com>
2023-03-27 02:57:43 +0300

    (add): tailwind config

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -2,8 +2,12 @@ import '../styles/home.css';
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline test">
-      Hello world!
-    </h1>
+    <div className='py-60 bg-primary'>
+      <div className='container mx-auto max-w-screen-xl'>
+        <h1 className="text-3xl font-bold text-white">
+          Hello world!
+        </h1>
+      </div>
+    </div>
   )
 }
